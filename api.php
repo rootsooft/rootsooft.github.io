ead10637df375aefded63dc98594dcb92d5d0565
rootsooft <100384536+rootsooft@users.noreply.github.com>
2023-05-11 19:31:05 +0200

    Add files via upload

---
+++ api.php
@@ -0,0 +1,13 @@
+ 
+
+{
+    "HD_VIDEO": [
+        {
+            "status": 1,
+            "apiKey": "viaviweb"
+			"message": "Invalid sign salt."
+			"package_name": "com.example.hdvideosapps"
+
+        }
+    ]
+}
